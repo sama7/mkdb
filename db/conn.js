a90b5923ecd32d3f0645c5ecede7a4cca72f2ee0
sama7 <samahbinsaeed@gmail.com>
2024-08-29 07:16:25 -0400

    update db connection for prod

--- db/conn.js
+++ db/conn.js
@@ -1,9 +1,11 @@
 import pg from 'pg';
 const { Pool } = pg;
 
+const dbUser = process.env.DB_USER || process.env.DEV_DB_USER;
+const dbPassword = process.env.DB_PASSWORD || process.env.DEV_DB_PASSWORD;
 const pool = new Pool({
-    user: 'samah',
-    password: process.env.DEV_DB_PASSWORD,
+    user: dbUser,
+    password: dbPassword,
     host: 'localhost',
     database: 'mkdb',
     port: process.env.DB_PORT || 5432,
