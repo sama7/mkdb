951dd87458cc45b4d239e06c181a47063c9e90c2
sama7 <samahbinsaeed@gmail.com>
2024-11-14 21:49:47 -0500

    add dropdown for pagination

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -9,12 +9,33 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
         if (currentPage < totalPages) onPageChange(currentPage + 1);
     };
 
+    const handlePageSelect = (e) => {
+        const selectedPage = parseInt(e.target.value, 10);
+        onPageChange(selectedPage);
+    };
+
     return (
         <div className="pagination">
             <button onClick={handlePrev} disabled={currentPage === 1}>
                 Previous
             </button>
-            <span>Page {currentPage} of {totalPages}</span>
+            <span>
+                Page 
+                <select
+                    name="current-page-select"
+                    id="current-page-select"
+                    value={currentPage}
+                    onChange={handlePageSelect}
+                    className="page-dropdown" 
+                >
+                    {[...Array(totalPages).keys()].map((page) => (
+                        <option key={page + 1} value={page + 1}>
+                            {page + 1}
+                        </option>
+                    ))}
+                </select>
+                of {totalPages} 
+            </span>
             <button onClick={handleNext} disabled={currentPage === totalPages}>
                 Next
             </button>
